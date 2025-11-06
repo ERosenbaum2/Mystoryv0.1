b9fc6d2f6c56b233fc9e3d5aa0063743a11719c3
ERosenbaum2 <ezrarosenbaum15@gmail.com>
2025-11-05 19:20:11 -0500

    Update model name for DALL-E image generation

--- project.py
+++ project.py
@@ -1559,9 +1559,10 @@ def generate_image_with_dalle(prompt_text, reference_image_path=None):
         
         # Use DALL-E 3 to generate image
         response = client.images.generate(
-            model="gpt-image-1",
+            model="dall-e-3",
             prompt=prompt_text,
             size="1024x1024",
+            quality="standard",
             n=1,
         )
         
